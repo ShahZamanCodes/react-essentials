f9150abbb8d8e49a383d9d27af0a5446466f4e26
ShahZamanCodes <manniee560@gmail.com>
2024-12-03 14:34:06 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='mb-4'>Hello World</h1>
+    <p>Shah Zaman</p>
+    </>
+  )
+}
+
+export default App
